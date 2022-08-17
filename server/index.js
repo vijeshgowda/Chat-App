d55c2d34800ae3f1b56d9cd8b0a8700254de59fa
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2022-08-18 00:07:44 +0100

    no idea

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const socketIO = require('socket.io')(http, {
     cors: {
-        origin: "http://localhostvijesh:3000"
+        origin: "http://localhost:3000"
     }
 });
 
@@ -39,27 +39,6 @@ socketIO.on('connection', (socket) => {
         socketIO.emit('newUserResponse', users);
         socket.disconnect();
     });
-});
-
-socketIO.on('connection', (socket) => {
-    // console.log(`⚡: ${socket.id} user just connected!`);
-    // socket.on('message', (data) => {
-    //   socketIO.emit('messageResponse', data);
-    // });
-
-    socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
-
-    // socket.on('newUser', (data) => {
-    //   users.push(data);
-    //   socketIO.emit('newUserResponse', users);
-    // });
-
-    // socket.on('disconnect', () => {
-    //   console.log('🔥: A user disconnected');
-    //   users = users.filter((user) => user.socketID !== socket.id);
-    //   socketIO.emit('newUserResponse', users);
-    //   socket.disconnect();
-    // });
 });
 
 app.get('/api', (req, res) => {
