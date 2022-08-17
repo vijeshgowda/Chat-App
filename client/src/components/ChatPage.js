c798278bebcf21af2052a493a14a3f08dc5cc873
vijeshgowda <88636289+vijeshgowda@users.noreply.github.com>
2022-08-18 00:14:40 +0100

    a lot of changes

--- client/src/components/ChatPage.js
+++ client/src/components/ChatPage.js
@@ -1,37 +1,35 @@
-import React, { useEffect, useState, useRef } from 'react';
-import ChatBar from './ChatBar';
-import ChatBody from './ChatBody';
-import ChatFooter from './ChatFooter';
+import React, { useEffect, useState, useRef} from 'react'
+import ChatBar from './ChatBar'
+import ChatBody from './ChatBody'
+import ChatFooter from './ChatFooter'
 
-const ChatPage = ({ socket }) => {
-    const [messages, setMessages] = useState([]);
-    const [typingStatus, setTypingStatus] = useState('');
+const ChatPage = ({socket}) => {
+    const [messages, setMessages] = useState([])
+    const [typingStatus, setTypingStatus] = useState("")
     const lastMessageRef = useRef(null);
 
-    useEffect(() => {
-        socket.on('messageResponse', (data) => setMessages([...messages, data]));
-    }, [socket, messages]);
+    useEffect(()=> {
+        socket.on("messageResponse", data => setMessages([...messages, data]))
+    }, [socket, messages])
+
+    useEffect(()=> {
+        socket.on("typingResponse", data => setTypingStatus(data))
+    }, [socket])
 
     useEffect(() => {
         // 👇️ scroll to bottom every time messages change
-        lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+        lastMessageRef.current?.scrollIntoView({behavior: 'smooth'});
     }, [messages]);
-
-    useEffect(() => {
-        socket.on('typingResponse', (data) => setTypingStatus(data));
-    }, [socket]);
 
     return (
         <div className="chat">
-            <ChatBar socket={socket} />
-            <div className="chat__main">
-                <ChatBody messages={messages}
-                          typingStatus={typingStatus}
-                          lastMessageRef={lastMessageRef} />
-                <ChatFooter socket={socket} />
+            <ChatBar socket={socket}/>
+            <div className='chat__main'>
+                <ChatBody messages={messages} typingStatus={typingStatus} lastMessageRef={lastMessageRef}/>
+                <ChatFooter socket={socket}/>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default ChatPage;
+export default ChatPage
